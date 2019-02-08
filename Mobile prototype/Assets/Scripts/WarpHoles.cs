74747f973af257d09624784319752a13112a5b89
Pratik Prasad Shringarpure <prati@DESKTOP-HI2QGSK>
2019-02-08 00:36:05 -0500

    Basic Warp Hole

---
+++ Mobile prototype/Assets/Scripts/WarpHoles.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpHoles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private RaycastHit hitinfo;
+
+    public GameObject currentplayer;
+
+    public GameObject warp_partner1;
+    private GameObject warp_position1;
+
+    public GameObject warp_partner2;
+    private GameObject warp_position2;
+
+
+    void Start()
+    {
+        warp_position1 = warp_partner1.transform.GetChild(0).gameObject;
+        warp_position2 = warp_partner2.transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hitinfo;
+           
+            if (Physics.Raycast(ray,out hitinfo))
+            {
+               if(hitinfo.collider.gameObject.tag == "Warphole")
+                {
+                    //Teleport Red Player to warp position;
+
+                    if(hitinfo.collider.gameObject.name == warp_partner1.gameObject.name)
+                    {
+                        //go to warp 2
+
+                        currentplayer.transform.position = warp_position2.transform.position;
+                    }
+
+                    else if(hitinfo.collider.gameObject.name == warp_partner2.gameObject.name)
+                    {
+
+                        //warp to warp1
+                        currentplayer.transform.position = warp_position1.transform.position;
+                    }
+                   
+                }
+            }
+        }
+    }
+      
+}
